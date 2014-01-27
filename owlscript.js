731160497e8d150816180f5b5f72635cdb48f393
Val Tenyotkin <val@tenyotk.in>
2014-01-27 12:07:05 -0800

    Initial commit.

---
+++ owlscript.js
@@ -0,0 +1,501 @@
+/*
+ * OwlScript v0.1 by Val Tenyotkin (val@tenyotk.in)
+ *
+ * Variables and properties prefixed with an underscore, though global, are
+ * internal and can be minified.  Global minification to common variable names
+ * such as a, b, c, etc. is not adviseable.  Convention of this file is to use
+ * a less common sequence _1, _2, _3, etc.
+ *
+ */
+
+/******************************************************************************/
+/*** Drawing ******************************************************************/
+/******************************************************************************/
+
+// canvas information object
+var _canvases = {};
+
+// ID of the current canvas
+var _current_canvas_id;
+
+// default drawing parameters
+var _default_color              = 'black';
+var _default_background_color   = 'white';
+var _default_line_width         = 1;
+
+// some recurring constants (for minification)
+var _tau        = 2*Math.PI;
+var _object     = 'object';
+var _undefined  = 'undefined';
+
+// dimensions of the current canvas
+var width, height;
+
+// animation frames
+var _frames = [];
+
+// flags
+var _animation_in_progress = 0;
+
+// self-explanatory
+function set_default_color(color) {
+    _canvases[_current_canvas_id]._color = color;
+}
+
+// self-explanatory
+function set_default_line_width(width) {
+    _canvases[_current_canvas_id]._line_width = width;
+}
+
+// self-explanatory
+function set_default_background_color(color) {
+    // store the color in the current canvas object
+    _canvases[_current_canvas_id]._background_color =
+    // change the style of the canvas
+    $(_current_canvas_id).style.backgroundColor = color;
+}
+
+// sets the current canvas and retrieves its defaults
+function set_canvas(id) {
+
+    // retrieve the current canvas ID
+    _current_canvas_id = id;
+
+    // if the canvas doesn't exist, create it
+    if(!_canvases[id]) {
+        // initiate the associative array
+        _canvases[id] = {};
+        // create the canvas object
+        _canvases[id]._canvas = $(id).getContext("2d");
+        // set the defaults
+        _canvases[id]._color             = _default_color;
+        _canvases[id]._line_width        = _default_line_width;
+        _canvases[id]._background_color  = _default_background_color;
+    }
+    // retrieve the dimensions of the canvas
+    width  = $(id).getAttribute('width');
+    height = $(id).getAttribute('height');
+}
+
+// if the width is supplied, return it
+// otherwise return the default
+function _get_default_line_width(width) {
+    return width || _canvases[_current_canvas_id]._line_width;
+}
+
+// if the color is supplied, return it
+// otherwise return the default
+function _get_default_color(color) {
+    return color || _canvases[_current_canvas_id]._color;
+}
+
+// self-explanatory
+function line(x0, y0, x1, y1, color, width) {
+    with(_canvases[_current_canvas_id]._canvas) {
+        beginPath();
+        lineWidth = _get_default_line_width(width);
+        strokeStyle = _get_default_color(color);
+        moveTo(x0, y0);
+        lineTo(x1, y1);
+        stroke();
+    }
+}
+
+// self-explanatory
+function circle(x, y, r, color, width) {
+    with(_canvases[_current_canvas_id]._canvas) {
+        beginPath();
+        lineWidth = _get_default_line_width(width);
+        strokeStyle = _get_default_color(color);
+        arc(x, y, r, 0, _tau, 0);
+        stroke();
+    }
+}
+
+// self-explanatory (filled circle)
+function disk(x, y, r, color) {
+    with(_canvases[_current_canvas_id]._canvas) {
+        beginPath();
+        fillStyle = _get_default_color(color);
+        arc(x, y, r, 0, _tau, 0);
+        fill();
+    }
+}
+
+// self-explanatory
+function rectangle(x0, y0, x1, y1, color, width) {
+    with(_canvases[_current_canvas_id]._canvas) {
+        beginPath();
+        lineWidth = _get_default_line_width(width);
+        strokeStyle = _get_default_color(color);
+        rect(x0, y0, x1-x0, y1-y0);
+        stroke();
+    }
+}
+
+// self-explanatory (filled recrangle)
+function sheet(x0, y0, x1, y1, color) {
+    with(_canvases[_current_canvas_id]._canvas) {
+        fillStyle = _get_default_color(color);
+        fillRect(x0, y0, x1-x0, y1-y0);
+    }
+}
+
+// self-explanatory
+function polygon(x, y, color, width) {
+    with(_canvases[_current_canvas_id]._canvas) {
+        beginPath();
+        lineWidth = _get_default_line_width(width);
+        strokeStyle = _get_default_color(color);
+        moveTo(x[0], y[0]);
+        for(var i = 1; i < x.length; i++)
+            lineTo(x[i], y[i]);
+        closePath();
+        stroke();
+    }
+}
+
+// self-explanatory
+function regular_polygon(x, y, r, n, color, width) {
+    var angles = range(0, 360*(1-1/n), 360/n);
+    polygon(
+        sin(angles).multiply( r).add(x),
+        cos(angles).multiply(-r).add(y),
+        color,
+        width);
+}
+
+// erase with the current background color
+function clear_canvas() {
+    sheet(0, 0, width, height, _canvases[_current_canvas_id]._background_color);
+}
+
+/******************************************************************************/
+/*** Animaiton ****************************************************************/
+/******************************************************************************/
+
+// makes a copy of a canvas and returns it
+function _clone_canvas(canvas) {
+    var new_canvas = document.createElement('canvas');
+    new_canvas.width  = canvas.width;
+    new_canvas.height = canvas.height;
+    new_canvas.getContext('2d').drawImage(canvas, 0, 0);
+    return new_canvas;
+}
+
+// self-explanatory
+function begin_animation() {
+    // create the _frames entry
+    _frames[_current_canvas_id] = [];
+}
+
+// store the current frame
+function new_frame() {
+    _frames[_current_canvas_id].push(_clone_canvas($(_current_canvas_id)));
+}
+
+// start the animation
+function animate(delay) {
+
+    // raise the animation flag
+    _animation_in_progress = 1;
+
+    // initiate the animation cycle at a default rate of 10 fps
+    _animation_cycle(_current_canvas_id, 0, delay*1000 || 100);
+}
+
+// animation cycle handler
+function _animation_cycle(id, index, delay) {
+
+    // display the current frame
+    _canvases[id]._canvas.drawImage(_frames[id][index % _frames[id].length],0,0);
+
+    // schedule the display of the next frame
+    setTimeout(function() {
+        _animation_cycle(id, index+1, delay);
+    }, delay);
+
+}
+
+/******************************************************************************/
+/*** Mathematics **************************************************************/
+/******************************************************************************/
+
+/*** Array Operations *********************************************************/
+
+for(var _name in _body = {
+    'add'       : function(a, b) { return a+b },
+    'subtract'  : function(a, b) { return a-b },
+    'multiply'  : function(a, b) { return a*b },
+    'divide'    : function(a, b) { return a/b },
+    'negative'  : function(a)    { return -a },
+    'minus'     : function(a)    { return -a }
+})
+    Array.prototype[_name] = (function(action) {
+        return function(x) {
+            for(var i = this.length, $ = []; i-->0;)
+                $[i] = action(this[i], typeof x === _object ? x[i % x.length] : x);
+            return $;
+        }
+    })(_body[_name]);
+
+// simplified loop prototype
+Array.prototype.loop = function(action) {
+    for(var i = 0, $ = []; i < this.length; i++)
+        $[i] = action(this[i]);
+    return $;
+}
+
+// redundant simplified loop prototype
+function loop(from, to, step, action) {
+
+    // no 'to' or 'step' implies that the function is to looped 'from' times
+    if(typeof to === 'function')
+        range(from).loop(to);
+    else
+        range(from, to, step).loop(action);
+}
+
+/*** Simplified Syntax Functions **********************************************/
+
+// define a universal argument function
+function _define(name, action) {
+
+    window[name] = function(x) {
+            var $ = [];
+            // if the argument is an array, perform action() on each element
+            if(typeof x === _object)
+                for(var i = x.length; i-->0; $[i] = action(x[i]));
+            // otherwise, i.e number or a string, return action(x)
+            else
+                $ = action(x);
+            return $;
+        };
+}
+
+for(var _name in _body = {
+    'abs'       : function(y) { return Math.abs(y) },
+    'sin'       : function(y) { return Math.sin(y*_tau/360) },
+    'cos'       : function(y) { return Math.cos(y*_tau/360) },
+    'round'     : function(y) { return Math.round(y) },
+    'sign'      : function(y) { return y < 0 ? -1 : 1 },
+    'sqrt'      : function(y) { return Math.sqrt(y) },
+    'sq'        : function(y) { return y*y },
+    'minus'     : function(y) { return -y },
+    'negative'  : function(y) { return -y }
+})
+    _define(_name, _body[_name]);
+/*
+    window[_name] = (function(action) {
+        return function(x) {
+            var $ = [];
+            // if the argument is an array, perform action() on each element
+            if(typeof x === _object)
+                for(var i = x.length; i-->0; $[i] = action(x[i]));
+            // otherwise, i.e number or a string, return action(x)
+            else
+                $ = action(x);
+            return $;
+        };
+    })(_body[_name]);
+*/
+// returns a "random" number between min and max
+//
+// if either of the arguments is a floating-point number, so is the returned
+// random number
+//
+// if the first and only argument is an array, return a random element from it
+function random(min, max) {
+
+    // if the argument is an array
+    if(typeof min === _object)
+        // return a random element from it
+        return min[random(0, min.length - 1)];
+
+    // random color special case
+    if(min === 'color') {
+        var $ = '0123456789abcdef'.split('');
+        return '#'+random($)+random($)+random($);
+    }
+
+    // generate a random number
+    var rnd = min + Math.random()*(max - min);
+
+    // round it if both arguments are integers
+    return min % 1 === 0 && max % 1 === 0 ? Math.round(rnd) : rnd;
+}
+
+// objectify x
+function _(x) {
+    return typeof x === _object ? x : [x];
+}
+
+// returns a minimum of any combination of the following arguments:
+//      number, number
+//      array,  number
+//      number, array
+//      array,  array
+function min(a, b) {
+
+    // convert a, and b if available, to an array
+    a = typeof b === _undefined ? _(a) : _(a).concat(b);
+
+    // find the minimum in the resulting array
+    for(var i = a.length, m = 1e50; i-->0;)
+        a[i] < m && (m = a[i]);
+
+    return m;
+}
+
+// clever max() made out of min()
+function max(a, b) {
+    return -min(minus(_(a)), minus(_(b)));
+}
+
+/******************************************************************************/
+/*** Miscellaneous ************************************************************/
+/******************************************************************************/
+
+// self-explanatory
+function set_title(title) {
+    document.title = title;
+}
+
+// print text
+function print(text, color) {
+
+    // convert to text
+    text += '';
+
+    // show text
+    $('_2_').innerHTML 
+        += '<div style="color:' +
+        _get_default_color(color)
+        +'">'
+        + text.replace(/\s/g, '&nbsp')
+        + '</div>';
+}
+
+// call the caller of this function again after a default delay of 1 second
+function repeat(delay) {
+    setTimeout(arguments.callee.caller, delay*1000 || 1);
+}
+
+// returns time in a neat object
+function time() {
+    var date = new Date();
+    return {
+        hour : date.getHours(),
+        minute : date.getMinutes(),
+        second : date.getSeconds(),
+        millisecond: date.getMilliseconds()
+    };
+}
+
+function range(a, b, step) {
+
+    // if b is missing, assume the range is from 1 to a
+    if(typeof b === _undefined) {
+        b = a;
+        a = 1;
+    }
+
+    // if the step is not defined or zero, set it to 1
+    step = abs(step) || 1;
+
+    // create the range
+    var r = [];
+
+    if(a < b)
+        for(; a <= b; a+= step)
+            r.push(a);
+    else
+        for(; b <= a; a-= step)
+            r.push(a);
+
+    return r;
+}
+
+// self-explanatory
+function $(id) {
+    return document.getElementById(id);
+}
+
+/******************************************************************************/
+/*** Parser *******************************************************************/
+/******************************************************************************/
+
+function _parse(code) {
+        // x { ==> function(x) {
+    return code.replace(/(\w)\s*{/g, 'function($1){')
+        // loop(50, { ==> loop(50, function(__x) {
+        .replace(/loop\s*\((.*,)\s*{/g, 'loop($1 function(__x){')
+        // f(x) = sin(x+1) ==> _define("f", function(x) { return sin(x); });
+        .replace(/(\w+)\s*\(\s*(\w+)\s*\)\s*=\s*(.*)/g, '_define("$1",function($2){return($3)});');
+}
+
+/******************************************************************************/
+/*** Bootstrap ****************************************************************/
+/******************************************************************************/
+
+// measure the screen and (re)draw
+function _initialize_image() {
+
+    // clear the screen
+    $(_current_canvas_id).width  = width  = window.innerWidth;
+    $(_current_canvas_id).height = height = window.innerHeight;
+
+    // clear the text screen
+    $('_2_').innerHTML = '';
+
+    // execute the code
+    main();
+}
+
+// self-explanatory
+window.onload = function() {
+
+    var body = document.getElementsByTagName('body')[0];
+
+    // create and ID the canvas element
+    body.appendChild(
+            document.createElement('canvas')
+        ).id = '_1_';
+    set_canvas('_1_');
+
+    // set the body margin to zero
+    body.style.margin = 0;
+
+    // create and ID the notebook element
+    var notebook = document.createElement('div');
+    notebook.id = '_2_';
+    notebook.style.position = 'absolute';
+    notebook.style.top = '0';
+    notebook.style.left = '0';
+    notebook.style.width = '100%';
+    notebook.style.height = '100%';
+    notebook.style.fontFamily = 'courier';
+    notebook.style.overflow = 'scroll';
+
+    body.appendChild(notebook);
+
+    /* parse the script */
+
+    // extract the code and delete the original script element
+    var input = $('owlscript');
+    var code = input.innerHTML;
+    input.parentNode.removeChild(input);
+
+    // create the new script element and parse the code
+    body.appendChild(document.createElement('script')).innerHTML = _parse(code);
+
+    // execute
+    _initialize_image();
+}
+
+// redraw on resize
+window.onresize = function() {
+    !_animation_in_progress && _initialize_image();
+}
+
+
